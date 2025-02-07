352586861e57e8eb98dea10bd7d45e0e91543096
chirag-2004 <chiraggupta.ddn@gmail.com>
2025-02-07 12:19:45 +0530

    Console FS modules

---
+++ Day1/p10.js
@@ -0,0 +1,4 @@
+let sum=(a,b)=>{return a+b}
+let diff=(a,b)=>{return a-b}
+// console.log(sum(15,17));
+module.exports={sum,diff};
---
+++ Day1/p11.js
@@ -0,0 +1,4 @@
+const sum=require("./p10.js");
+
+console.log(sum(5,7));
+console.log(diff(10,20));
