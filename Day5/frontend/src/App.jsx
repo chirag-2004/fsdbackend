874dff53fbeb93551f217e73655ab5fe637161b3
chirag-2004 <chiraggupta.ddn@gmail.com>
2025-03-28 11:22:56 +0530

    User Registration App

---
+++ Day5/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Register from './components/Register'
+
+const App = () => {
+  return (
+    <div>
+      <h1>User Registration Form</h1>
+      <Register/>
+    </div>
+  )
+}
+
+export default App
