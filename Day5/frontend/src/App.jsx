fe1f5bc7dfe768261267321ac84626dc192657c8
chirag-2004 <chiraggupta.ddn@gmail.com>
2025-03-28 12:19:25 +0530

    User Registration App

--- Day5/frontend/src/App.jsx
+++ Day5/frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Register from './components/Register'
+import View from './components/view'
 
 const App = () => {
   return (
     <div>
       <h1>User Registration Form</h1>
       <Register/>
+      <View/>
     </div>
   )
 }
